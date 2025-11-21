0f81611a396a60b3e98cb7b1809493e4534ca1c0
Chaima Bouallegue <chaimabouallegue17@gmail.com>
2025-11-21 13:57:04 +0100

    Ajout du gitignore et nettoyage des fichiers Terraform ignorés

---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "La région AWS"
+  type        = string
+  default     = "us-east-1"  # Région mise à jour
+}
+
+variable "cluster_name" {
+  description = "Nom du cluster EKS"
+  type        = string
+  default     = "mykubernetes"  # Nom du cluster mis à jour
+}
+
+variable "subnet_ids" {
+  description = "IDs des sous-réseaux"
+  type        = list(string)
+  default     = ["subnet-0de96e04e6978aeea", "subnet-0605ef5fce965abc4"]  # Valeurs par défaut
+}
+
+variable "role_arn" {
+  description = "ARN du rôle IAM pour EKS"
+  type        = string
+  default     = "arn:aws:iam::862116348508:role/LabRole"  # Valeur par défaut
+}
+
+variable "vpc_id" {
+  description = "L'ID du VPC pour le cluster EKS"
+  type        = string
+  default     = "vpc-0eb8a940c43ab9440"  # Remplacez par votre ID de VPC réel
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"  # Modifiez-le selon vos besoins
+}
